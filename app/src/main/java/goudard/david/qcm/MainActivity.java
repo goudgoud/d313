1a68052cdde14d5901463c6ac6c4362b7325b7f2
David GOUDARD <david@goudard.web4me.fr>
2016-12-19 20:38:09 +0100

    first commit

---
+++ app/src/main/java/goudard/david/qcm/MainActivity.java
@@ -0,0 +1,13 @@
+package goudard.david.qcm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
