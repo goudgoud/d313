7c24c3e82ba5b43794226b0b1a75b991e77bd45c
David GOUDARD <david@goudard.web4me.fr>
2016-12-23 19:45:37 +0100

    add SurveyFamilyActivity

---
+++ app/src/main/java/goudard/david/qcm/SurveyFamilyActivity.java
@@ -0,0 +1,11 @@
+package goudard.david.qcm;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by david on 23/12/16.
+ */
+
+public class SurveyFamilyActivity extends AppCompatActivity {
+
+}
